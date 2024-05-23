66acbf32f598c06aeb53188013eae68a2bec2989
ahskdjahskdaj8ry923 <sturley.zachary@gmail.com>
2024-05-23 02:24:27 -0400

    Update thing1.js

--- pages/thing1.js
+++ pages/thing1.js
@@ -1,1 +1,17 @@
+import Head from 'next/head';
 
+export default function Thing1() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Thing 1</title>
+        <meta name="description" content="Welcome to Page 1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/styles.css" /> {/* Include your CSS */}
+      </Head>
+      <main>
+        <h1>Welcome to Page 1</h1>
+      </main>
+    </div>
+  );
+}
