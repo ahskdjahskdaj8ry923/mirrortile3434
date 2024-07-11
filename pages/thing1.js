5a6d0c8000b8e3f5b8c9ee7a07a8ed43f85650a0
ahskdjahskdaj8ry923 <sturley.zachary@gmail.com>
2024-07-10 23:34:21 -0400

    Update thing1.js

--- pages/thing1.js
+++ pages/thing1.js
@@ -5,12 +5,12 @@ export default function Thing1() {
     <div className="container">
       <Head>
         <title>Thing 1</title>
-        <meta name="description" content="Welcome to Page 1" />
+        <meta name="description" content="Welcome to the Thing 1 Page" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/styles.css" /> {/* Include your CSS */}
       </Head>
       <main>
-        <h1>Welcome to Page 2</h1>
+        <h1>Welcome to Thing 1 Page</h1>
       </main>
     </div>
   );
